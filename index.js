92edf825a6367092afd4166f3ebfc2c682420696
Don Lin <don.lin@optimalworkshop.com>
2025-01-09 09:09:16 +1300

    ensure server is running

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 const {ApolloServer} = require("apollo-server");
 
 const server = new ApolloServer({typeDefs, resolvers});
+
+server.listen().then(({url}) => {
+  console.log(`Server ready at ${url}`);
+});
